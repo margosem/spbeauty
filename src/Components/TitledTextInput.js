538154e35b12123b80788c750da4b7e3546ed62f
margosem <167997911+margosem@users.noreply.github.com>
2024-04-26 20:06:23 +0000

    26.04

--- src/Components/TitledTextInput.js
+++ src/Components/TitledTextInput.js
@@ -3,9 +3,10 @@ import styled from "styled-components";
 const Input = styled.input`
     border-radius: 24px;
     right: 16px;
-    border: solid #E2E8F0 1px ;
-    background: #F8FAFC);
+    border: solid #E2E8F0 1px;
+    background: #F8FAFC;
     color: #0F172A;
+    padding: 8px 16px;
     &:hover {
         border-radius: 24px;
         border: solid #E2E8F0 1px;
@@ -18,18 +19,22 @@ const Input = styled.input`
         border: solid #E2E8F0 1px;
         opacity: 0.8;
         background: #F8FAFC;
-    
     }
 `;
 
+const Title = styled.div`
+    color: #94A3B8;
+    font-size: caption-rg;
+`;
+
 function TitledTextInput({title, def}) {
-    const Title = title === undefined ? 
+    const TitleComponent = title === undefined ? 
         (<div></div>) : 
-        (<div>{title}</div>);
+        (<Title>{title}</Title>);
     return (
-        <div className='d-flex flex-column gap-2'>
-            {Title}
-            <Input className='caption-rg p-2' defaultValue={def} ></Input>
+        <div className='d-flex flex-column gap-2'> {/* уменьшено расстояние между заголовком и селектом */}
+            {TitleComponent}
+            <Input className='text-rg' defaultValue={def}></Input>
         </div>
     );
 }
